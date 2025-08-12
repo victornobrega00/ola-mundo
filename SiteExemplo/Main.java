df89af2d8e061991b3bf2991452e418ad84c0ddb
victornobrega00 <victornobrega007@gmail.com>
2025-08-12 20:37:58 -0300

    atualização na parte de java

--- SiteExemplo/Main.java
+++ SiteExemplo/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("One more test in GitHub web");
+        System.out.printl("mais um teste");
     }
 }
