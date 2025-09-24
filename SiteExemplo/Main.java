d3a2a7ca839a39f020c9c7447ea70464e0fdd103
victornobrega00 <victornobrega007@gmail.com>
2025-09-24 08:05:12 -0300

    Add age calculation based on user input
    Introduced a method to calculate age from the current year and year of birth, and updated the main method to prompt the user for input and display the calculated age.

--- SiteExemplo/Main.java
+++ SiteExemplo/Main.java
@@ -1,10 +1,20 @@
 package SiteExemplo;
+import java.util.Scanner;
 
 public class Main {
+
+    public static int idade (int anoAtual, int anoNascimento){
+        return anoAtual - anoNascimento;
+    }
     public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("One more test in GitHub web");
-        System.out.println("mais um teste");
-        System.out.println("usando o github web");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o ano que estamos:");
+        int anoAtual = sc.nextInt();
+        System.out.println("Digite o ano do seu nascimento: ");
+        int anoNascimento = sc.nextInt();
+
+        int idade = idade(anoAtual, anoNascimento);
+        System.out.println("Sua idade é: " + idade);
     }
 }
